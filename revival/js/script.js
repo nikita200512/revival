81f41134101c824b9909c30e6a1a3dd08683eef8
nikita200512 <93154095+nikita200512@users.noreply.github.com>
2023-01-02 18:33:23 +0300

    Add files via upload

---
+++ revival/js/script.js
@@ -0,0 +1,9 @@
+// $('.header__menu-btn').on('click', function() {
+//   $('.header__menu-list').toggleClass('header__menu-list--active');
+// });
+
+$(function() {
+  $('.header__menu-btn').on('click', function() {
+    $('.header__menu-list').toggleClass('header__menu-list--active')
+  });
+});
